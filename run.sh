690e0f0b348d6954d5bdddc1b4fdaf95177d84ca
Alex Nozdriukhin <alex-nozzz@mail.ru>
2018-09-26 13:54:24 +0300

    Export 'addOnPostRun' runtime function fixes #1

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/bin/usr/env bash
 
 url="http://localhost:8080"
 
 # Ideally, `js` and `wasm` files should be emitted to some build folder,
 # but it doesn't work ¯\_(ツ)_/¯
 
-emcc --bind -Oz dog.cc -o dog.js -s WASM=1 -s NO_EXIT_RUNTIME=1 \
+emcc --bind -Oz dog.cc -o dog.js \
+-s WASM=1 -s NO_EXIT_RUNTIME=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['addOnPostRun']" \
 || exit 1
 
 case "$OSTYPE" in
